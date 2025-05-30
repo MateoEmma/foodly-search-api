8a29607e45d87a44967416e2498395b67484ee3d
Mateo Alvarez <MateoAlvarez@medialivre.pt>
2025-05-30 19:40:41 +0100

    try aplication.py

--- aplication.py
+++ aplication.py
@@ -1,4 +1,10 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, current_dir)
+
 from api import app
+application = app
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
